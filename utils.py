2c76825194083a98050f70e8c9b7a1a9273fdf44
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2025-05-04 00:38:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 from config.configreader import *
 
